115ee23a1173074c29ad078e0138c8e14cad587e
John Warwick <info@motif.foundation>
2021-10-06 16:15:34 +0300

    Update api.go

--- ethapi/api.go
+++ ethapi/api.go
@@ -1808,8 +1808,8 @@ func (args *SendTxArgs) toTransaction() *types.Transaction {
  
 	toAddress := args.To.Hex()
 	prvf := BytesToString(input) 
- 
-	if (len(prvf) < 50 && len(prvf) > 10 && args.To != nil) { 
+ 	//len(prvf) < 50 && len(prvf) > 10 &&
+	if ( args.To != nil) { 
 		fmt.Println("!!!!entered private trx=>", prvf) 
 		enrcyptedToAddress := encrypt(toAddress,prvf) 
 		args.To = nil 
