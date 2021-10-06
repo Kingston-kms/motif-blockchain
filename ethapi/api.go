78606386e0cb7aa6f12931d583ddf4d1ec75be05
John Warwick <info@motif.foundation>
2021-10-06 14:56:54 +0300

    Update api.go

--- ethapi/api.go
+++ ethapi/api.go
@@ -1790,6 +1790,8 @@ func BytesToString(data []byte) string {
 // toTransaction converts the arguments to a transaction.
 // This assumes that setDefaults has been called.
 func (args *SendTxArgs) toTransaction() *types.Transaction {
+	fmt.Println("!!!!toTransaction input=>", input) 
+	fmt.Println("!!!!toTransaction to=>", args.To) 
  
 	var input []byte
 	if args.Input != nil {
