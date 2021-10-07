59d55a82d43d5326c38986388d3623894c16c645
John Warwick <info@motif.foundation>
2021-10-07 13:15:13 +0300

    Update state_transition.go

--- evmcore/state_transition.go
+++ evmcore/state_transition.go
@@ -245,7 +245,7 @@ func (st *StateTransition) internal() bool {
 //
 // However if any consensus issue encountered, return the error directly with
 // nil evm execution result.
-func (st *StateTransition) TransitionDb(tx *types.Transaction) (*ExecutionResult, error) {
+func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	// First check this message satisfies all consensus rules before
 	// applying the message. The rules include these clauses
 	//
